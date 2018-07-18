35335cc296c896165a5a7850d34eaf3bb25da087
J. Maina Karuoya <jmaina12@gmail.com>
2018-07-18 09:57:25 +0300

    Rewrite
    Final major update to the code.

--- projectCamera.py
+++ projectCamera.py
@@ -1,103 +1,90 @@
-from tkinter import *
+import serial
 from datetime import datetime as dt
-"""import serial
-from gpiozero import LED
 from picamera import PiCamera
-from time import sleep"""
+from signal import pause
+from time import sleep
 
-"""camera = PiCamera()
+#this creates the camera object
+camera = PiCamera()
 camera.rotation = 180
-i = 0
-j = 0
-#LED Light objects created
-blue = LED(17)
-white = LED(27)
 
-serialTrigger = serial.Serial('/dev/ttyACMO', 9600)
-serialTrigger.baudrate = 9600"""
-#Don't forget to change the directory path
-#as well as some of the sections with triple quotes!
+#the port from which the serial input comes from, as well as the baudrate
+serialTrigger = serial.Serial("/dev/ttyACM0" , 9600)
 
-#this section defines the name (title) and size of our GUI
-window = Tk()
-window.title("Motion Activated Security Camera")
-window.geometry('350x300')
+#the function that reads the serial input, called by the trigger function
+def readSerialInput():
+    serialTrigger.flushInput()
+    readSerialTrigger = serialTrigger.readline()
+    print(readSerialTrigger)
+    return True
 
-#the functions called by the buttons directly are specified here
-def triggerFunction():
-    print("All systems go!")
-    readSerialInput()
-
-def logFileButton():
-    buttonPressed = True
-    if (buttonPressed == True):
-        viewLogFile()
-
-def videoCapture():
-    print("Video here!")
-    """global j
-    j += 1
-    camera.start_preview()
-    camera.start_recording('/home/pi/Desktop/image_%s.jpg' % i)
-    camera.stop_recording()
-    camera.stop_preview()
-    print("Video Capture Successful.")
-    updateLogFile()
-    #lightBlinks()"""
-
-#here we create our buttons, and set them out on our grid
-btn = Button(window, text = "Activate Picture Mode", padx = 5, pady = 5,
-             command = triggerFunction)
-btn.grid(column = 0, row = 0)
-btnTwo = Button(window, text = "Activate Video Mode", padx = 5, pady = 5,
-                command = videoCapture)
-btnTwo.grid(column =0, row = 4)
-btnThree = Button(window, text = "Read Security Log", padx = 5, pady = 5,
-                  command = logFileButton)
-btnThree.grid(column = 0, row = 8)
-btnFour = Button(window, fg = "red", text = "Quit!",
-                 command = quit, padx = 5, pady = 5)
-btnFour.grid(column = 0, row = 12)
-
-#the functions come here
+#this function captures still images
 def cameraCapture():
-    print("Camera here!")
-    camera.start_preview()
+    camera.start_preview(alpha=150)
     sleep(2)
-    global i
-    i += 1
-    camera.capture('/home/pi/Desktop/image_%s.jpg' % i)
+    x = dt.now().strftime("%Y%m%d_%H%M%S")
+    camera.capture('/home/pi/Desktop/Projects/image%s.jpg' % x)
     camera.stop_preview()
-    #LED blinks to indicate capture successful
     print("Camera capture successful.")
-    #lightBlinks()
 
+#this function allows the user to read the contents of the log file
 def viewLogFile():
     logfile = open("securitylog.txt", mode = 'r', encoding = 'utf-8')
     for line in logfile:
         print(line, end = '')
-    print("Will view log file here.")
 
+#this function updates the security log file
 def updateLogFile():
     with open("securitylog.txt",'a',encoding = 'utf-8') as logfile:
-        logfile.write("Motion detected at " + dt.now().strftime("%m-%d-%Y, %H:%M"))
-    #print("Updating log file..")
+        logfile.write("Motion detected at " + dt.now().strftime("%m-%d-%Y, %H:%M") + "\n")
 
-def readSerialInput():
+#this function calls the fuction capturing images or recording videos;
+#calls the serial reading function and uses the user´s input to determine the mode
+def triggerFunction():
     print("Reading serial input..")
-    """readSerialTrigger = serialTrigger.readline()
-    if (readSerialTrigger == "Motion detected!"):
-        cameraCapture()
-        updateLogFile()"""
+    if (readSerialInput() == True): 
+        if (userInput == 1):
+            cameraCapture()
+            updateLogFile()
+        elif(userInput == 2):
+            videoCapture()
+            updateLogFile()
+        else:
+            print("Error! Try again.")
 
-"""def lightBlinks():
-    blue.blink()
-    for i in range(3):
-        white.on()
-        sleep(0.3)
-        white.off()
-        sleep(0.3)"""
+#this function captures video
+def videoCapture():
+    print("Video here!")
+    y = dt.now().strftime("%Y%m%d_%H%M%S")
+    camera.start_preview(alpha=150)
+    camera.start_recording('/home/pi/Desktop/Projects/video%s.h264' % y)
+    sleep(10)
+    camera.stop_recording()
+    camera.stop_preview()
+    print("Video Capture Successful.")
+    updateLogFile()
 
-#this is the command that enables the user to view the GUI.
-#all functions come in here.
-window.mainloop()
+#sort of like the ´main´ function
+while True:
+    print("---")
+    print("---Motion Activated Security Camera System---")
+    print("Here are the options:")
+    print("1. Activate system in camera mode.")
+    print("2. Activate system in video mode.")
+    print("3. View security log file.")
+    print("4. Quit")
+    userInput = int(input("Choose the mode you want: "))
+
+    if (userInput == 1):
+        triggerFunction()
+    elif(userInput == 2):
+        triggerFunction()
+    elif(userInput == 3):
+        viewLogFile()
+    elif(userInput == 4):
+        quit()
+    else:
+        print("Error!")
+        
+#asynchronous serial transmission
+pause()
