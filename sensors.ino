f79e7d8f20842857b6bba1fb1804fd12dbc8c15b
mainakaruoya <35466955+mainakaruoya@users.noreply.github.com>
2018-07-18 10:10:17 +0300

    [Probably the] Last Edit
    Only one line will be transmitted over the serial cable, not two.

--- sensors.ino
+++ sensors.ino
@@ -7,7 +7,7 @@
 int ledPin = 13;                // choose the pin for the LED
 int inputPin = 5;               // this is the input pin for the first PIR sensor
 int inputPinTwo = 7;            //this is the input pin for the second PIR sensor
-int pirState = LOW;             // for both sensors, we start by assuming no motion detected
+int pirState = LOW;             // for both sensors, we start by assuming that their state is 'no motion detected'
 int pirTwoState = LOW;         
 int val = 0;                    // this variable and the one below it are there to read the pin status
 int val2 = 0;
@@ -15,7 +15,7 @@ int val2 = 0;
 void setup() {
   
   pinMode(ledPin, OUTPUT);      // declare LED as output (there is one on the board that is installed; in any case one can manually attach an LED to pin 13)
-  pinMode(inputPin, INPUT);     // declare sensors as input (this one and the one below it)
+  pinMode(inputPin, INPUT);     // declare sensor pins as input (this one and the one below it)
   pinMode(inputPinTwo, INPUT);
  
   Serial.begin(9600); //baudrate
@@ -29,7 +29,7 @@ void loop(){
     if ((pirState == LOW) && (pirTwoState == LOW)) {
       // we have just turned on
       Serial.println(data);
-      // We only want to print on the output change, not state
+      // We only want to print on the output change, not state change
       pirState = HIGH;
       pirTwoState = HIGH;
     }
@@ -37,8 +37,7 @@ void loop(){
     digitalWrite(ledPin, LOW); // turn LED OFF
     if ((pirState == HIGH) && (pirTwoState == HIGH)){
       // we have just turned off
-      Serial.println("Motion ended!");
-      // We only want to print on the output change, not state
+      //Serial.println("Motion ended!");
       pirState = LOW;
       pirTwoState = LOW;
     }
